f3397ef5a742b248c46cb7b01395a73b8bd16605
dlmarinho <marinhodaniel71@gmail.com>
2024-03-09 13:30:21 -0300

    Projeto de funções completo

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+let resultado = nivelReanked (25, 5)
+console.log(resultado)
+
+function nivelReanked (vitorias, derrotas) {
+    let saldo = saldoVitorias(vitorias, derrotas)
+    let nivel
+    if (saldo <= 10 ) {
+        nivel = "Ferro"
+    }else if ((saldo >= 11) && (saldo <= 20)) {
+        nivel = "bronze"
+    }else if ((saldo >= 21) && (saldo <= 50)) {
+        nivel = "Prata"
+    }else if ((saldo >= 51) && (saldo <= 80)) {
+        nivel = "Ouro"
+    }else if ((saldo >= 81) && (saldo <= 90)) {
+        nivel = "Diamante"
+    }else if ((saldo >= 91) && (saldo <= 100)) {
+        nivel = "Lendário"
+    }else{
+        nivel = "Imortal"
+    }
+
+    return `O Herói tem de saldo de ${saldo} está no nível de ${nivel}`
+}
+
+
 function saldoVitorias (vitorias, derrotas) {
-    saldo = vitorias - derrotas
+    let saldo = vitorias - derrotas
     return saldo
  }
+
